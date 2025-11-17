df29b4f4b0949a8785e04e9724f6591341e15e19
lilac02leila <lailakudo@gmail.com>
2025-11-17 14:42:00 +0100

    Modified loader and requirements files

--- src/loader.py
+++ src/loader.py
@@ -5,7 +5,7 @@ class DocumentLoader:
     def __init__(self, data_dir: str, file_type: str = "pdf"):
         self.data_dir = data_dir
         self.file_type = file_type
-
+        
     def load_documents(self):
         if self.file_type == "pdf":
             loader = DirectoryLoader(self.data_dir, glob="*.pdf", loader_cls=PyPDFLoader)
