611fa74b2b935719c0fd968bf7543959b6dd4fd7
lilac02leila <lailakudo@gmail.com>
2025-11-17 15:24:25 +0100

    Update Loader

--- src/loader.py
+++ src/loader.py
@@ -1,11 +1,11 @@
 #TODO: implementer loader
-from langchain.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 
 class DocumentLoader:
     def __init__(self, data_dir: str, file_type: str = "pdf"):
         self.data_dir = data_dir
         self.file_type = file_type
-        
+
     def load_documents(self):
         if self.file_type == "pdf":
             loader = DirectoryLoader(self.data_dir, glob="*.pdf", loader_cls=PyPDFLoader)
